ac28d5ac9dc9fd6c92a2643464cf6f40ccd573eb
DiegoGalma <diegogalmarini@gmail.com>
2026-02-19 09:21:58 +0100

    Restoration Complete: Modernized code, restored assets, updated DB, and fixed media serving

--- nycs/urls.py
+++ nycs/urls.py
@@ -91,5 +91,11 @@ urlpatterns += [
     url(r'^tinymce/', include('tinymce.urls')),
 ]
 
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    from django.views.static import serve
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
